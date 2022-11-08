e3cce71f9dfcfef3964e6cfd895017cb73e00a84
Team VR7 <96851821+Ahmedmfdh@users.noreply.github.com>
2022-11-08 04:04:05 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -37,7 +37,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME","gohara")
 
 # ايديهات المطورين الملاك
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "").split())
+    map(int, getenv("OWNER_ID", "5648157552").split())
 ) 
 
 # سيبو زي مهوه ملهوش لزمه
